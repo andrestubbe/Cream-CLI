744ad4bbcdb3b3cee2380308735f875a97d044b3
Andre Stubbe <andrestubbe@proton.me>
2026-08-16 22:04:17 +0200

    chore: hide all source code - keep only README.md and docs/

--- src/main/java/cream/cli/view/editor/syntax/SyntaxHighlighter.java
+++
@@ -1,9 +0,0 @@
-package cream.cli.view.editor.syntax;
-
-public interface SyntaxHighlighter {
-    int[] highlight(String text);
-
-    default byte[] highlightStyles(String text) {
-        return new byte[text.length()];
-    }
-}
